19cd74210123c125898b43a5c522f3149b49cf3f
David Bond <david.bond@panoramicdata.com>
2024-04-10 20:55:38 +0100

    Improved column type handling

--- MicrosoftAzureSentinel.Api/Models/QueryResponse.cs
+++ MicrosoftAzureSentinel.Api/Models/QueryResponse.cs
@@ -25,10 +25,26 @@ public class QueryResponse
 					}
 
 					var value = row[i] is JsonElement element ? element : throw new InvalidOperationException("Deserialization failure.");
+					var column = columns[i];
 					row[i] = value.ValueKind switch
 					{
-						JsonValueKind.String => value.GetString(),
-						JsonValueKind.Number => value.GetDouble(),
+						JsonValueKind.String => column.Type switch
+						{
+							"datetime" => value.GetDateTime(),
+							"datetimeoffset" => value.GetDateTimeOffset(),
+							"guid" => value.GetGuid(),
+							"string" => value.GetString(),
+							_ => value.GetString()
+						},
+						JsonValueKind.Number => column.Type switch
+						{
+							"long" => value.GetInt64(),
+							"int" => value.GetInt32(),
+							"short" => value.GetInt16(),
+							"byte" => value.GetByte(),
+							"float" => value.GetSingle(),
+							_ => value.GetDouble()
+						},
 						JsonValueKind.True or JsonValueKind.False => value.GetBoolean(),
 						JsonValueKind.Object => value,
 						JsonValueKind.Array => value,
