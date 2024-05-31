ec350b8f9e17c597f104a2b0d3903898397afc87
David Bond <david.bond@panoramicdata.com>
2024-05-31 15:58:24 +0100

    Removed non-functional expand parameter

--- MicrosoftAzure.Api.Test/ResourceGroups/ResourceGroupTests.cs
+++ MicrosoftAzure.Api.Test/ResourceGroups/ResourceGroupTests.cs
@@ -18,24 +18,4 @@ public class ResourceGroupTests(ITestOutputHelper testOutputHelper) : TestBase(t
 			response.Values.Should().NotBeNullOrEmpty();
 		}
 	}
-
-	[Fact]
-	public async Task GetResourceGroupsAsync_WithExpand_Succeeds()
-	{
-		// Get subscription ids
-		foreach (var subscriptionId in await GetSubscriptionIdsAsync(default).ConfigureAwait(true))
-		{
-			var response = await Client
-				.ResourceGroups
-				.GetAsync(
-					subscriptionId,
-					expand: "resources",
-					cancellationToken: default)
-				.ConfigureAwait(true);
-
-			response.Should().NotBeNull();
-			response.Values.Should().NotBeNull();
-			response.Values.Should().OnlyContain(x => x != null);
-		}
-	}
 }
--- MicrosoftAzure.Api/Interfaces/IResourceGroups.cs
+++ MicrosoftAzure.Api/Interfaces/IResourceGroups.cs
@@ -10,10 +10,4 @@ public interface IResourceGroups
 	Task<PlainResponse<ResourceGroup>> GetAsync(
 		Guid subscriptionId,
 		CancellationToken cancellationToken);
-
-	[Get("/subscriptions/{subscriptionId}/resourcegroups?api-version=2024-03-01")]
-	Task<PlainResponse<ResourceGroup>> GetAsync(
-		Guid subscriptionId,
-		[AliasAs("$expand")] string? expand = default,
-		CancellationToken cancellationToken = default);
 }
